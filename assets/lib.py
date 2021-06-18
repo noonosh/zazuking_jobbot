af34760129c00f3c3da1cd34d27fd1a6bbd551a4
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-06-18 16:24:47 +0500

    Add info about vacancies

---
+++ assets/lib.py
@@ -0,0 +1,31 @@
+vacancy = {
+    "assistant": {
+        "word": "Я ассистент",
+        "link": "https://docs.google.com/forms/d/e/1FAIpQLSedovQBYg4beJfrvUdp0qKDi9pL9LmYZx1knnKTPkf1IfA2Bw/viewform"
+    },
+    "manager": {
+        "word": "Я менеджер детского сада",
+        "link": "https://docs.google.com/forms/d/e/1FAIpQLSc_eqc7ZnXGGI-dDdcqviAMGYcZwxPr4zeEKn-900dYxRpSJA/viewform"
+    },
+    "educator": {
+        "word": "Я воспитатель детского сада",
+        "link": "https://docs.google.com/forms/d/e/1FAIpQLSfGBEgeB6cdBORpuaQfyBuV8iwdncXp2THU4G3-FDM1fPRuyg/viewform"
+    },
+    "teacher": {
+        "word": "Я учитель почемучки",
+        "link": "https://docs.google.com/forms/d/e/1FAIpQLSdOuj2pxKJt1QIpgqytpFen3WvlEdcWRYjK1XXpXvzUxJ7k0w/viewform"
+    },
+    "nanny": {
+        "word": "Я няня детского сада",
+        "link": "https://docs.google.com/forms/d/e/1FAIpQLSdI7A1XFq53tzvpSKF9Mzp0hWDC3hFU4vwz0eeVvQCT79Rjog/viewform"
+    },
+    "project_manager": {
+        "word": "Я проектный менеджер",
+        "link": "https://docs.google.com/forms/d/e/1FAIpQLSd-6stiDndQMtjMEXK50ltoS58KvLkQ6OrOG68uF3cb2LWb1Q/viewform"
+    },
+    "why_manager": {
+        "word": "Я менеджер почемучки",
+        "link": "https://docs.google.com/forms/d/e/1FAIpQLSdA1cyNuhLN8zemGr2frA7V6WrrXhkIAkMxN0OfBWOx0UI6tw/viewform"
+    }
+
+}
