a67a30a36914efab1c64445e07b7a0c066145e08
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-06-18 11:58:30 +0500

    First command

---
+++ callbacks/greeting.py
@@ -0,0 +1,4 @@
+
+
+def start(update, context):
+    pass
--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 import os
+import logging
+from dotenv import load_dotenv
+from telegram.ext import CommandHandler, Updater
+from callbacks.greeting import *
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
 
 
 def main():
-    pass
+    load_dotenv()
+    updater = Updater(token=os.getenv('API_TOKEN'))
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', start))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
